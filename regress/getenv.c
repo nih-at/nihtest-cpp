304409539b28882f5a8a6b2c85800d3e0a4185a2
Dieter Baron <dillo@nih.at>
2020-06-03 17:06:44 +0200

    Enclose value in double qoutes, as expected by test.

--- regress/getenv.c
+++ regress/getenv.c
@@ -44,7 +44,12 @@ int main(int argc, char *argv[]) {
     for (int i=1; i<argc; i++) {
 	char *ret;
 	ret = getenv(argv[i]);
-	printf("%s=%s\n", argv[i], ret ? ret : "<NULL>");
+        if (ret != NULL) {
+            printf("%s=\"%s\"\n", argv[i], ret);
+        }
+        else {
+            printf("%s=<null>\n", argv[i]);
+        }
     }
     return 0;
 }
