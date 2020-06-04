85fb4f5c69e30ee5aedffe71d0342c2c85a5f8e7
Thomas Klausner <tk@giga.or.at>
2020-06-04 10:40:08 +0200

    Improve comments, try to make it more C++, losing some readability.

--- src/CompareArrays.cc
+++ src/CompareArrays.cc
@@ -52,13 +52,13 @@ bool CompareArrays::compare_quiet() {
     if (expected.size() != got.size()) {
         return false;
     }
-    
+
     for (size_t i = 0; i < expected.size(); i++) {
         if (expected[i] != got[i]) {
             return false;
         }
     }
-    
+
     return true;
 }
 
@@ -72,57 +72,79 @@ bool CompareArrays::compare_verbose() {
     /* Algorithm from
      * Eugene W. Myers - A O(ND) Difference algorithm and Its Variations
      */
+    /* The graph G consists of nodes [x, y] where x is the index into
+     * the array of lines "expected" and y is the index into the array
+     * of lines "got". G(x,y) always has outgoing edges to G(x+1, y)
+     * and G(x, y+1) with weight 1. Such an edge corresponds to
+     * accepting the line expected[x] (or got[y] resp.) as different
+     * and printing it. Additionally, an edge G(x, y) -> G(x+1, y+1)
+     * with weight 0 exists if expected[x] is equal to got[y]. Going
+     * along such an edge corresponds to finding a matching line in
+     * expected and got.
+     *
+     * The problem of finding the smallest diff corresponds to finding a
+     * directed path from G(0, 0) to G(N, M) with minimal weight. */
+
+    /* did we find a path from G(0, 0) to G(N, M)? */
     bool done = false;
+    /* x dimension of G */
     const int N = static_cast<int>(expected.size());
+    /* y dimension of G */
     const int M = static_cast<int>(got.size());
-    /* maximum size of differences accepted (could be set lower than M + N) */
+    /* upper limit on the number of differences this algorithm accepts
+     * before giving up. If set to M+N like here it will never give up
+     * and always find a path. */
     const int max_size = M + N;
-    int w[2*max_size+1];
-    /* for retracing our steps and getting the actual diff */
+    /* v[k] = highest x coordinate we can reach after d non-diagonal
+     * steps, where y = x - k */
+    /* v is updated in-place in each iteration */
+    /* The array v in the paper is [-max_size ... +max_size]; w is v
+       rebased by max_size, i.e. v[k] = w[max_size + k] */
+    std::vector<int> w(2*max_size+1);
+
+    /* keep copies of v/w from previous iterations for retracing our
+     * steps and getting the actual diff */
     std::vector<std::vector<int> > path_lengths;
-    /* re-index w so that v[-max_size .. max_size] are valid */
-    /* v[k] = highest x we can reach after d non-diagonal steps, where y = x - k */
-    int *v = w+max_size;
+    /* number of non-diagonal steps needed = number of differences
+     * between expected and got */
     int d;
 
-    /* initialize variable, only needed for debugging */
-    for (int i=0; i<2*max_size+1; i++) {
+    /* initialize w, only needed for debugging */
+    for (int i=0; i<w.size(); i++) {
 	w[i] = 999999999;
     }
 
-    /* no differences before start of file */
-    v[1] = 0;
-    /* loop over all possible diagonals */
+    /* Initialize v[1] for first step of algorithm */
+    w[max_size+1] = 0;
+    /* loop over all possible differences, stop on the lowest one where we reach G(N, M) */
     for (d=0; d <= max_size; d++) {
-	/* update path lengths */
+	/* update v_d[k] using v_{d-1}[k] - can be done in-place, so only v[k] is used here */
 	for (int k=-d; k<=d; k+=2) {
-	    /* find best candidate for shortest path */
-            /*
-             * x -- offset into first array (expected), <= N
-             * y -- offset into second array (got), <= M
-	     */
 	    int x, y;
-	    
-	    if (k == -d || (k != d && v[k-1] < v[k+1])) {
+	
+	    if (k == -d || (k != d && w[max_size+k-1] < w[max_size+k+1])) {
 		/* step down, x does not increase */
-		x = v[k+1];
+		x = w[max_size+k+1];
 	    } else {
 		/* step right, x increases */
-		x = v[k-1]+1;
+		x = w[max_size+k-1]+1;
 	    }
 	    y = x - k;
-	    /* go diagonally as far as possible */
+	    /* follow free edges (diagonal steps) as far as possible */
 	    while (x < N && y < M && expected[x] == got[y]) {
-		/* both lines are equal, so shortest path doesn't grow */
+		/* both lines are equal, so weight of path doesn't increase */
 		x++;
 		y++;
 	    }
-	    v[k] = x;
+	    w[max_size+k] = x;
+	    /* did we reach G(N, M)? if yes, stop */
 	    if (x >= N && y >= M) {
 		done = true;
 		break;
 	    }
 	}
+
+        /* save v_d[k] for generating diff */
 	std::vector<int> dest;
 	//	std::cout << "array of level " << d << ": ";
 	//int j = -max_size;
@@ -141,7 +163,7 @@ bool CompareArrays::compare_verbose() {
     }
     if (done) {
 	output(path_lengths, max_size, d, N, M);
-    }		
+    }
     return false;
 }
 
@@ -150,7 +172,7 @@ void CompareArrays::output(std::vector<std::vector<int> > &path_lengths, int max
     if (d == 0) {
 	return;
     }
-    
+
     auto wdprev = path_lengths[d-1];
     auto k = x - y;
     bool change;
@@ -159,6 +181,7 @@ void CompareArrays::output(std::vector<std::vector<int> > &path_lengths, int max
      * going forward we might have entered in the middle of one */
     do {
 	change = false;
+	/* did we step right in the previous iteration? */
 	if (x == wdprev[max_size + k - 1] + 1) {
 	    output(path_lengths, max_size, d-1, x-1, y);
 	    print_line('-', expected[x-1]);
@@ -167,6 +190,7 @@ void CompareArrays::output(std::vector<std::vector<int> > &path_lengths, int max
 	    }
 	    return;
 	}
+	/* did we step down in the previous iteration? */
 	if (x == wdprev[max_size + k + 1]) {
 	    output(path_lengths, max_size, d-1, x, y-1);
 	    print_line('+', got[y-1]);
@@ -175,9 +199,9 @@ void CompareArrays::output(std::vector<std::vector<int> > &path_lengths, int max
 	    }
 	    return;
 	}
-	/* if possible, walk one step backward on the diagonal and try again */
+	/* no match, so try going up diagonally one step and trying again */
 	if (x > 0 && y > 0 && expected[x-1] == got[y-1]) {
-	    /* for context */
+	    /* unchanged lines, for context */
 	    lines.insert(lines.begin(), expected[x-1]);
 	    x--;
 	    y--;
@@ -194,6 +218,6 @@ void CompareArrays::print_line(char indicator, const std::string &line) {
         std::cout << "Unexpected " << what << ":\n";
         printed_header = true;
     }
-    
+
     std::cout << indicator << line << "\n";
 }
